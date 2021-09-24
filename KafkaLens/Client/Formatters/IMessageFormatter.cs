a93b78fdcfe61a0b4dfe36b323c8563e10e5551c
Pravin Chaudhary <pravin.chaudhary@live.com>
2021-09-24 15:26:02 +0530

    Added json formatter

---
+++ KafkaLens/Client/Formatters/IMessageFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaLens.Client.Formatters
+{
+    public interface IMessageFormatter
+    {
+        string DisplayName { get; set; }
+
+        string Format(string data);
+    }
+}
