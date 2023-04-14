b2c321b86b2357ddaf45c14b7339fe3457b6c0ae
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2023-04-15 01:04:26 +0530

    Added support for integer message keys

--- Shared/Models/Message.cs
+++ Shared/Models/Message.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace KafkaLens.Shared.Models;
@@ -18,10 +20,51 @@ public class Message
         Headers = headers;
         Key = key;
         Value = value;
-        KeyText = key == null ? "" : Encoding.Default.GetString(key);
-        ValueText = this.Value == null ? "" : Encoding.Default.GetString(this.Value);
+        KeyText = GetKeyText(key);
+        ValueText = Value == null ? "" : Encoding.Default.GetString(Value);
     }
-    
+
+    private static string GetKeyText(byte[]? bytes)
+    {
+        if (bytes == null)
+        {
+            return "";
+        }
+        
+        var stringValue = Encoding.ASCII.GetString(bytes);
+        if (stringValue.ToCharArray().ToList().TrueForAll(IsAscii))
+        {
+            return stringValue;
+        }
+        var intValue = GetIntValue(bytes);
+        if (intValue != 0)
+        {
+            return intValue.ToString();
+        }
+
+        return stringValue;
+    }
+
+    private static int GetIntValue(byte[] bytes)
+    {
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(bytes);
+        }
+
+        int intValue = BitConverter.ToInt32(bytes, 0);
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(bytes);
+        }
+        return intValue;
+    }
+
+    private static bool IsAscii(char obj)
+    {
+        return obj >= 32 && obj <= 126;
+    }
+
     public byte[]? Key { get; }
 
     public byte[]? Value { get; }
