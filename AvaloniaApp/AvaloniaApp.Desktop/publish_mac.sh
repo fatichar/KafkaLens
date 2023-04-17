36c0b130e6f4d2df8d7dfc753a5f8dc3008eee60
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2023-04-18 00:23:37 +0530

    Renamed publish directories

--- AvaloniaApp/AvaloniaApp.Desktop/publish_mac.sh
+++ AvaloniaApp/AvaloniaApp.Desktop/publish_mac.sh
@@ -1,3 +1,3 @@
 ﻿#! /bin/bash -x
 
-dotnet publish AvaloniaApp.Desktop.csproj -c Release --sc -r osx-x64 -p:PublishTrimmed=false -o ../../Releases/MacOS/
+dotnet publish AvaloniaApp.Desktop.csproj -c Release --sc -r osx-x64 -p:PublishTrimmed=false -o ../../Releases/KafkaLens_mac/
