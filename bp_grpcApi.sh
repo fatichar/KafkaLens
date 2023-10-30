884d15e44202cddc6ffabb0594bf3d3cc3317c53
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2023-10-30 12:06:44 +0530

    Update build files

--- bp_grpcApi.sh
+++ bp_grpcApi.sh
@@ -1,15 +1,13 @@
-# if [ -z "$TAG" ]
-# then
-#     TAG=$({ date '+%Y%m%d%H%M%S' && echo - && git rev-parse --short HEAD && echo - && git branch | grep \* | cut -d ' ' -f2; } | tr -d '\n')
-#     echo "Using TAG: "$TAG
-# fi
+if [ -z "$TAG" ]
+then
+   TAG=$({ date '+%Y%m%d%H%M%S' && echo - && git rev-parse --short HEAD && echo - && git branch | grep \* | cut -d ' ' -f2; } | tr -d '\n')
+   echo "Using TAG: ""$TAG"
+fi
 
-# SERVICE=kafkalens_grpc_api
+SERVICE=kafkalens_grpc_api
 
-# echo "Building and deploying" $SERVICE
+echo "Building and pushing" $SERVICE
 
-TAG=20221208205859-14824b9-grpc
+docker build -f Dockerfile_grpcApi . -t $SERVICE:"$TAG"
 
-# docker build -f Dockerfile_grpcApi . -t $SERVICE:$TAG
-
-../../tarana/projects/common/scripts/release_docker_to_artifactory.sh pravin.chaudhary AKCp5emG5o82vbeocyNmCAZ83UDaAqBbitEr7XYf1LBoKMPsFqAy2BvKadf6qGMMxxMYBvvur $SERVICE $TAG
+../tcc/common/scripts/release_docker_to_artifactory.sh pravin.chaudhary AKCp5emG5o82vbeocyNmCAZ83UDaAqBbitEr7XYf1LBoKMPsFqAy2BvKadf6qGMMxxMYBvvur $SERVICE $TAG
--- build_grpc_api.sh
+++ build_grpc_api.sh
@@ -1,1 +1,7 @@
-docker build -f Dockerfile_grpcApi . -t kafkalens_grpc_api
+if [ -z "$TAG" ]
+then
+   TAG=$({ date '+%Y%m%d%H%M%S' && echo - && git rev-parse --short HEAD && echo - && git branch | grep \* | cut -d ' ' -f2; } | tr -d '\n')
+   echo "Using TAG: "$TAG
+fi
+
+docker build -f Dockerfile_grpcApi . -t kafkalens_grpc_api:"$TAG"
