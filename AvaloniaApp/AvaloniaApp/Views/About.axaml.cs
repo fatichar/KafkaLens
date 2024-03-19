4640eee41fe92e618797cba8c21e7d096541d97e
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2024-03-19 13:11:58 +0530

    Updated "About" dialog
    - added hyperlnks for product page and github page

--- AvaloniaApp/AvaloniaApp/Views/About.axaml.cs
+++ AvaloniaApp/AvaloniaApp/Views/About.axaml.cs
@@ -1,5 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using ActiproSoftware.UI.Avalonia.Controls;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace AvaloniaApp.Views;
@@ -12,5 +17,36 @@ public partial class About : Window
 #if DEBUG
         this.AttachDevTools();
 #endif
+    }
+
+    private void Url_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is HyperlinkTextBlock { CommandParameter: string url })
+        {
+            OpenUrl(url);
+        }
+    }
+    
+    public void OpenUrl(object urlObj)
+    {
+        var url = urlObj as string;
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            //https://stackoverflow.com/a/2796367/241446
+            using var proc = new Process { StartInfo = { UseShellExecute = true, FileName = url } };
+            proc.Start();
+
+            return;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("x-www-browser", url);
+            return;
+        }
+
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) throw new ArgumentException("invalid url: " + url);
+        Process.Start("open", url);
+        return;
     }
 }
