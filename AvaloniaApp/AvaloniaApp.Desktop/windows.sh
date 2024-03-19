1375835ab5d8a7aa5f18bcc490b777f56b6f4659
Pravin Chaudhary <pravin.chaudhary@gmail.com>
2024-03-19 15:16:05 +0530

    Add windows installer

--- AvaloniaApp/AvaloniaApp.Desktop/windows.sh
+++ AvaloniaApp/AvaloniaApp.Desktop/windows.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -x
 
-dotnet publish AvaloniaApp.Desktop.csproj -c Release --sc -r win-x64 -p:PublishTrimmed=false -o ../../Releases/KafkaLens_win/
+dotnet publish AvaloniaApp.Desktop.csproj -c Release -r win-x64 --sc -p:PublishTrimmed=false -o ../../Releases/KafkaLens_win/
