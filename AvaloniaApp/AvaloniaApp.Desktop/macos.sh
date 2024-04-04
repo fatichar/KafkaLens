f3a0e80d36042cc6a9e9d9d8d1229028436ee40e
Pravin Chaudhary <pravin.chaudhary@taranawireless.com>
2024-04-04 16:33:00 +0530

    Updated os id for mac in publish script

--- AvaloniaApp/AvaloniaApp.Desktop/macos.sh
+++ AvaloniaApp/AvaloniaApp.Desktop/macos.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -x
 
-dotnet publish AvaloniaApp.Desktop.csproj -c Release --sc -r osx.11.0-arm64 -p:PublishTrimmed=false -o ../../Releases/KafkaLens_mac/
+dotnet publish AvaloniaApp.Desktop.csproj -c Release --sc -r osx-arm64 -p:PublishTrimmed=false -o ../../Releases/KafkaLens_mac/
